43f4b9197b1a281ceac04c2ff7a210aa6b7af4c8
rafoshi <rafa.ioshi@gmail.com>
2023-08-29 19:06:41 -0300

    Nvim config

---
+++ lua/theprimeagen/packer.lua
@@ -0,0 +1,20 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+	end})
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+end)
